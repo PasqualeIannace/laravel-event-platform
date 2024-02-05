4c214bab156049e0a2d2bc43aeb446ab97c94deb
PasqualeIannace <pasquale.iannace@gmail.com>
2024-02-05 22:27:38 +0100

    Aggiunti tag seeders

---
+++ config/tags.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    [
+        'sponsor' => 'Coca Cola',
+    ],
+    [
+        'sponsor' => 'Pirelli',
+    ],
+    [
+        'sponsor' => 'Master Card',
+    ],
+    [
+        'sponsor' => 'Nike',
+    ],
+    [
+        'sponsor' => 'Red Bull',
+    ],
+    [
+        'sponsor' => 'Fed Ex',
+    ],
+];
--- database/seeders/TagSeeder.php
+++ database/seeders/TagSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Tag;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,6 +15,13 @@ class TagSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $tags = config("tags");
+
+        foreach ($tags as $tag) {
+            $new_tag = new Tag();
+
+            $new_tag->sponsor = $tag["sponsor"];
+            $new_tag->save();
+        }
     }
 }
